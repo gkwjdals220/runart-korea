bab6c8e6f9643300b6630530559f45696541a582
gkwjdals220 <gkwjdals220@gmail.com>
2026-08-31 23:58:03 +0900

    Rename visible brand to TTWITTUN

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Brand({small=false}:{small?:boolean}){
   return <Link className="brand" href="/">
-    <Image className="brandLogo" src="/ttunttun-logo.jpeg" width={small?48:62} height={small?48:62} alt="뛰뚠뛰뚠 실제 크루 로고"/>
-    <div><b>RUNART KOREA</b><span>TTUNTTUN RUNNING CREW</span></div>
+    <Image className="brandLogo" src="/ttunttun-logo.jpeg" width={small?48:62} height={small?48:62} alt="TTWITTUN 러닝 크루 로고"/>
+    <div><b>TTWITTUN</b><span>RUNNING · DISCOVER · RECORD</span></div>
   </Link>
 }
