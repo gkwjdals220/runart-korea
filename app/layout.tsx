79ed2d1d8fcad0d9cf04bc1b69989baea90bb797
gkwjdals220 <gkwjdals220@gmail.com>
2026-09-02 13:07:36 +0900

    Load live race calendar styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "./mobile-pages.css";
 import "./mobile-overlap-fixes.css";
 import "./mobile-explorer-nav.css";
 import "./mobile-explorer-refine.css";
+import "./race-live.css";
 import "leaflet/dist/leaflet.css";
 import MobileBottomNav from "@/components/MobileBottomNav";
 import UrgentFacilityNav from "@/components/UrgentFacilityNav";
