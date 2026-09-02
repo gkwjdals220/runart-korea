a37eed906deff46fce20b4ac8f8542ddcfe3c3d2
gkwjdals220 <gkwjdals220@gmail.com>
2026-09-02 12:18:41 +0900

    Load compact mobile explorer and tab bar styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import "./ui-fixes.css";
 import "./board-home.css";
 import "./mobile-pages.css";
 import "./mobile-overlap-fixes.css";
+import "./mobile-explorer-nav.css";
 import "leaflet/dist/leaflet.css";
 import MobileBottomNav from "@/components/MobileBottomNav";
 import UrgentFacilityNav from "@/components/UrgentFacilityNav";
