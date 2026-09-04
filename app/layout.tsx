6bddfe7803861b67080dbecfb8b25580ff746487
gkwjdals220 <gkwjdals220@gmail.com>
2026-09-05 02:05:23 +0900

    Load race list v2 styles last

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ import "./final-ui-polish.css";
 import "./race-layout-fixes.css";
 import "./header-reset.css";
 import "./race-card-mobile-refine.css";
+import "./race-list-v2.css";
 import { Noto_Sans_KR } from "next/font/google";
 import MobileBottomNav from "@/components/MobileBottomNav";
 import UrgentFacilityNav from "@/components/UrgentFacilityNav";
