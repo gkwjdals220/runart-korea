339479f0855f36b4e7d6a8d41c9569050480eba4
gkwjdals220 <gkwjdals220@gmail.com>
2026-09-09 19:22:58 +0900

    Load compact PB icon styles last

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ import "./mobile-input-fixes.css";
 import "./header-reset.css";
 import "./home-banner-final.css";
 import "./manage-hub-polish.css";
+import "./pb-icon-fix.css";
 import { Noto_Sans_KR } from "next/font/google";
 import MobileBottomNav from "@/components/MobileBottomNav";
 import UrgentFacilityNav from "@/components/UrgentFacilityNav";
